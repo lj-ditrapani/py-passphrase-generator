fc6ae3cc1aea84d9a293037545e377718288d33f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-09-14 23:05:29 -0400

    feat: add argument parsing & help via argparse

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+import argparse
 import secrets
 import sys
 
-def gen_passphrase(word_count: int = 7, separator: str = '-'):
-    f = open('bitcoin-bip39.txt', 'r')
-    text = f.read()
-    f.close()
-    lines = [l.strip() for l in text.strip().split('\n')]
+def gen_passphrase(word_count: int, separator: str, file_name: str):
+    with open(file_name, 'r') as file:
+        text = file.read()
+    lines = [line.strip() for line in text.strip().splitlines()]
+    count = len(lines)
     def gen_word():
-        i = secrets.randbelow(2048)
+        i = secrets.randbelow(count)
         return lines[i]
     words = [gen_word() for n in range(word_count)]
     print(separator.join(words))
 
-try:
-    gen_passphrase(int(sys.argv[1]), sys.argv[2])
-except IndexError:
-    try:
-        gen_passphrase(int(sys.argv[1]))
-    except IndexError:
-        gen_passphrase()
+parser = argparse.ArgumentParser()
+parser.add_argument("count", type=int, help='number of random words to generate')
+parser.add_argument("separator", type=str, help="string to insert between words. Use ' ' for empty space")
+parser.add_argument("--file", "-f", type=str, help="source dictonary file", default='bitcoin-bip39.txt')
+args = parser.parse_args()
+gen_passphrase(args.count, args.separator, args.file)
