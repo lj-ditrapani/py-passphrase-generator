b10625ee7acb1cfa9435518bdc68f8ce65a1405f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-09-14 23:16:04 -0400

    feat: change file param to built-in wordlist
    This seems more ergonomic to just type in the file prefix of a built-in
    word list then to have the user provide files.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import argparse
 import secrets
 import sys
 
-def gen_passphrase(word_count: int, separator: str, file_name: str):
-    with open(file_name, 'r') as file:
+def gen_passphrase(word_count: int, separator: str, file_prefix: str):
+    with open(file_prefix + '.txt', 'r') as file:
         text = file.read()
     lines = [line.strip() for line in text.strip().splitlines()]
     count = len(lines)
@@ -16,6 +16,6 @@ def gen_passphrase(word_count: int, separator: str, file_name: str):
 parser = argparse.ArgumentParser()
 parser.add_argument("count", type=int, help='number of random words to generate')
 parser.add_argument("separator", type=str, help="string to insert between words. Use ' ' for empty space")
-parser.add_argument("--file", "-f", type=str, help="source dictonary file", default='bitcoin-bip39.txt')
+parser.add_argument("--wordlist", "-l", type=str, help="source word list from bitcoin or eff.org", default='bip39', choices=['bip39', 'eff-large', 'eff-short', 'eff-short-2'])
 args = parser.parse_args()
-gen_passphrase(args.count, args.separator, args.file)
+gen_passphrase(args.count, args.separator, args.wordlist)
